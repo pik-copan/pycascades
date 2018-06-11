7f5f6e4b0585dda8f9ce59a279e67ba6f460df69
Jonathan Krönke <jkroenke@uni-potsdam.de>
2018-06-11 11:29:41 +0200

    [WIP] Implement integration of scipy ode solver.

--- modules/core/network.py
+++ modules/core/network.py
@@ -1,24 +1,29 @@
 class network:
-	def __init__(self):
-		self.tip_list = []
-	def add_element(self,tip_element):
-		self.tip_list.append(tip_element)
-	def plot_net(self):
-		ret_string = ""
-		for tip_element in self.tip_list:
-			ret_string = ret_string + str(tip_element.id) + "\n"
-			for cpl in tip_element.cpl_list:
-				ret_string = (ret_string + str(cpl.out.id) 
-					      + " --> " + str(tip_element.id) 
-					      + "\n")
-		return ret_string
-	def iterate(self,dt):
-		for tip_element in self.tip_list:
-			tip_element.iterate(dt)
-		for tip_element in self.tip_list:
-			tip_element.push_state()
-	def get_state(self):
-		ret_string = ""
-		for tip_element in self.tip_list:
-			ret_string = ret_string + str(tip_element.x) + " "
-		return ret_string
+    
+    def __init__(self):
+        self.tip_list = []
+        
+    def add_element(self,tip_element):
+        self.tip_list.append(tip_element)
+        
+    def plot_net(self):
+        ret_string = ""
+        for tip_element in self.tip_list:
+            ret_string = ret_string + str(tip_element.id) + "\n"
+        for cpl in tip_element.cpl_list:
+            ret_string = (ret_string + str(cpl.out.id) 
+                          + " --> " + str(tip_element.id)
+				        + "\n")
+        return ret_string
+    
+    def system(self,x,t):
+        df = []
+        for tip_element in self.tip_list:
+            df.append(tip_element.f_prime())
+        return df
+            
+    def get_state(self):
+        state_vec = []
+        for tip_element in self.tip_list:
+            state_vec.append(tip_element.x)
+        return state_vec
--- modules/core/tipping_element.py
+++ modules/core/tipping_element.py
@@ -1,11 +1,13 @@
 class tipping_element:
+    
 	def __init__(self,id_number):
 		self.id = id_number
 		self.x = 0.0
-		self.x_new = self.x
 		self.cpl_list = []
+        
 	def add_cpl(self,cpl):
 		self.cpl_list.append(cpl)
+        
 	def cpl_sum(self):
 		try: 
     			self.cpl_list
@@ -16,18 +18,24 @@ class tipping_element:
 			for linear_coupling in self.cpl_list:
 				ret_sum = ret_sum + linear_coupling.coupling()
 			return ret_sum 
+        
 	def dxdt(self):
-		print "Warning: Either using abstract tipping_element class which is not suggested or forgot to override dxdt() function"
+		print ("Warning: Either using abstract tipping_element class which"
+                 + "is not suggested or forgot to override dxdt() function")
 		return 0.0
-	def iterate(self,dt):
-		self.x_new = self.x + dt * ( self.dxdt() + self.cpl_sum() )
+    
+	def f_prime(self):
+		return ( self.dxdt() + self.cpl_sum() )
+    
 	def push_state(self):
 		self.x = self.x_new
 
 class cusp(tipping_element):
+    
 	def __init__(self, id_number, x, a):
 		tipping_element.__init__(self,id_number)
 		self.x = x
 		self.a = a
+        
 	def dxdt(self):
 		return -pow(self.x,3)+self.a*self.x
