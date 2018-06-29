16ab062be526b3cf67b4d6d96c9ebc9ed95afcdf
Jonathan Krönke <kroenke@pik-potsdam.de>
2018-06-29 14:32:54 +0200

    Add tip method to solver class.

--- modules/core/integrate.py
+++ modules/core/integrate.py
@@ -14,6 +14,8 @@ class solver():
         self.states = [self.net.get_state()]
         self.times = [0]
         self.pars = []
+        self.r = ode(self.net.f_prime).set_integrator('vode', method='bdf')
+        self.r.set_initial_value(self.net.get_state(),self.times[-1])
         
     def _iterate(self,t_end,t_step):
         """Private iterate method for constant parameter"""
@@ -51,19 +53,28 @@ class solver():
             self.pars.append(te.c)
             te.c += par_step
 
+    def tip(self,tip_id_list,tolerance,t_step,realtime_break=None):
+        continue_flag = True
+        while continue_flag:
+            self.equilibrate(tolerance,t_step,realtime_break)
+            for id in tip_id_list:
+                self.net.node[id]['data'].c+=0.01*t_step
+            continue_flag=False
+            for id in tip_id_list:
+                if not self.net.node[id]['data'].tipped:
+                    continue_flag=True
+                       
     def equilibrate(self,tolerance,t_step,realtime_break=None):
         """Iterate system until it is in equilibrium.
         TODO: Add stability checkup of fixpoint (with jacobian)"""
-        r = ode(self.net.f_prime).set_integrator('vode', method='bdf')
-        r.set_initial_value(self.net.get_state(),self.times[-1])
         break_flag = False
         t0 = time.process_time()
-        while r.successful() and not break_flag:
-            r.integrate(r.t+t_step)
-            self.net.set_state(r.y)
-            self.states.append(r.y)
-            self.times.append(r.t)
-            fix_point = np.less(np.abs(self.net.f_prime(r.t,r.y))
+        while self.r.successful() and not break_flag:
+            self.r.integrate(self.r.t+t_step)
+            self.net.set_state(self.r.y)
+            self.states.append(self.r.y)
+            self.times.append(self.r.t)
+            fix_point = np.less(np.abs(self.net.f_prime(self.r.t,self.r.y))
                                 ,tolerance*np.ones((1
                                 ,self.net.number_of_nodes())))
             
--- scripts/cusp_chain.py
+++ scripts/cusp_chain.py
@@ -12,7 +12,7 @@ from gen.net_factory import net_factory
 from core.integrate import solver
 
 net_factory = net_factory()
-net = net_factory.create_cusp_chain(5,-1,1,0,-1,0.34)
+net = net_factory.create_cusp_chain(5,-1,1,0,-1,0.35)
 
 t0 = time.process_time()
 pos=nx.spring_layout(net)
@@ -27,12 +27,9 @@ solver = solver(net)
 
 # initialize time
 t0 = time.process_time()
-solver.equilibrate(0.001,0.01,0.3)
+solver.tip([0],0.005,0.1,30)
 t_iteration = time.process_time() - t0
 print(t_iteration)
-
-print(solver.times)
-print(solver.states)
 
 plt.figure(1)
 plt.plot(solver.times,solver.states)
